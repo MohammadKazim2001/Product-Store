26c4517430b70cf5858c9db78a3ea83807657693
Kazim Amiri <mohammad.kazimamiri2001@gmail.com>
2025-05-22 11:15:46 +0430

    product model was set

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,40 @@
-// const express = require("express");
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
+// console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
+  connectDB();
   console.log("Server stated at http://localhost:5000");
 });
+
+// mohammadkazimamiri2001
+// SYi6Q6qUn6TtUt9z
